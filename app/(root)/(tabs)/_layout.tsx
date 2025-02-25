98e3d45711e701f5b2a715bf93209b50e2978c8b
Lemuel Velez <lemuel@seabudigital.com>
2025-02-26 01:44:26 +0800

    feat: add status bar in layout

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from "expo-router";
-import { Image, View } from "react-native";
+import { Image, View, StatusBar } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const TabIcon = ({
@@ -59,87 +59,90 @@ const TabsLayout = () => {
   const insets = useSafeAreaInsets();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarShowLabel: false,
-        tabBarStyle: {
-          backgroundColor: "#188E54",
-          position: "absolute",
-          height: 65 + insets.bottom,
-          paddingBottom: insets.bottom,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabIcon
-              focused={focused}
-              icon={require("../../../assets/images/Home.png")}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="inbox"
-        options={{
-          title: "Inbox",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabIcon
-              focused={focused}
-              icon={require("../../../assets/images/Inbox.png")}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="qrcode"
-        options={{
-          title: "QR Code",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabIcon
-              focused={focused}
-              icon={require("../../../assets/images/QRcode.png")}
-              isQR={true}
-            />
-          ),
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#188E54" />
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
           tabBarStyle: {
-            display: "none", // Hides the tab bar when on the QR Code screen
+            backgroundColor: "#188E54",
+            position: "absolute",
+            height: 65 + insets.bottom,
+            paddingBottom: insets.bottom,
           },
         }}
-      />
-      <Tabs.Screen
-        name="transaction"
-        options={{
-          title: "Transaction",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabIcon
-              focused={focused}
-              icon={require("../../../assets/images/Transaction.png")}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabIcon
-              focused={focused}
-              icon={require("../../../assets/images/Profile.png")}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon
+                focused={focused}
+                icon={require("../../../assets/images/Home.png")}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="inbox"
+          options={{
+            title: "Inbox",
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon
+                focused={focused}
+                icon={require("../../../assets/images/Inbox.png")}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="qrcode"
+          options={{
+            title: "QR Code",
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon
+                focused={focused}
+                icon={require("../../../assets/images/QRcode.png")}
+                isQR={true}
+              />
+            ),
+            tabBarStyle: {
+              display: "none", // Hides the tab bar when on the QR Code screen
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="transaction"
+          options={{
+            title: "Transaction",
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon
+                focused={focused}
+                icon={require("../../../assets/images/Transaction.png")}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon
+                focused={focused}
+                icon={require("../../../assets/images/Profile.png")}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 };
 
