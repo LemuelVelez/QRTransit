581523c56e0354127855186948adbd75a51ccab9
lemuel <pluklem07@gmail.com>
2025-09-18 15:14:22 +0800

    fix: css-interop + reanimated v3 plugin; drop worklets

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,11 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-      presets: [
-        ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-        'nativewind/babel', // NativeWind v4 is a *preset*
-      ],
-      plugins: [
-        'expo-router/babel',
-        'react-native-reanimated/plugin', // <-- LAST for Reanimated v3
-      ],
+        presets: ['babel-preset-expo'],
+        plugins: [
+            'expo-router/babel',
+            'react-native-css-interop/babel', // NativeWind v4 uses css-interop under the hood
+            'react-native-reanimated/plugin', // LAST for Reanimated v3
+        ],
     };
-  };
-  
+};
