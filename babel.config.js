2471e4d24514f6f14e0990286db4b6cdbe669341
lemuel <pluklem07@gmail.com>
2025-09-18 14:22:33 +0800

    fix(babel): correct presets/plugins; worklets last

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,13 @@
+// babel.config.js
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: [
-            ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-            'expo-router/babel',
-            "nativewind/babel",
-            'react-native-worklets/plugin',
-        ],
+      presets: ['babel-preset-expo'],
+      plugins: [
+        'expo-router/babel',
+        'nativewind/babel',         // keep only if you use NativeWind
+        'react-native-worklets/plugin', // MUST be last
+      ],
     };
-};
+  };
+  
