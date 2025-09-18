7f3fd4d1ccb851cc732dcb848a150b9d84343cd0
lemuel <pluklem07@gmail.com>
2025-09-18 15:50:26 +0800

    chore: pin react-native-css-interop@0.1.22 for RN 0.76 + Reanimated v3

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,14 @@
-// babel.config.js
 module.exports = function (api) {
     api.cache(true);
     return {
       presets: [
-        ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-        'nativewind/babel', // NativeWind v4 is a *preset*
+        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+        "nativewind/babel",
       ],
-      // No plugins needed on SDK 52 (expo-router & reanimated are included by the preset)
+      plugins: [
+        "expo-router/babel",
+        "react-native-reanimated/plugin", // keep LAST
+      ],
     };
   };
   
