e1ac986dbc03ef8fa7ce311d07ba888eed2d14a5
lemuel <pluklem07@gmail.com>
2025-09-18 13:26:36 +0800

    chore(config): clean up babel/metro configs and update package-lock

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,6 @@
-// metro.config.js
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
-// Make sure the CSS file below exists.
-module.exports = withNativeWind(config, { input: './app/globals.css' });
+module.exports = withNativeWind(config, { input: './app/globals.css' })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-  presets: [require('nativewind/preset')],
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      fontFamily: {
-        rubik: ['Rubik-Regular', 'sans-serif'],
-        'rubik-bold': ['Rubik-Bold', 'sans-serif'],
-        'rubik-extrabold': ['Rubik-ExtraBold', 'sans-serif'],
-        'rubik-light': ['Rubik-Light', 'sans-serif'],
-        'rubik-medium': ['Rubik-Medium', 'sans-serif'],
-        'rubik-semibold': ['Rubik-SemiBold', 'sans-serif'],
-      },
+      rubik: ['Rubik-Regular', 'sans-serif'],
+      "rubik-bold": ['Rubik-Bold', 'sans-serif'],
+      "rubik-extrabold": ['Rubik-ExtraBold', 'sans-serif'],
+      "rubik-light": ['Rubik-Light', 'sans-serif'],
+      "rubik-medium": ['Rubik-Medium', 'sans-serif'],
+      "rubik-semibold": ['Rubik-SemiBold', 'sans-serif'],
     },
   },
   plugins: [],
-};
+}
