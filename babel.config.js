2d8310f3977684d3fe59337dee9ecd5270e17fa5
lemuel <pluklem07@gmail.com>
2025-09-18 15:03:19 +0800

    chore: drop react-native-worklets; use reanimated v3 plugin

--- babel.config.js
+++ babel.config.js
@@ -3,16 +3,12 @@ module.exports = function (api) {
     api.cache(true);
     return {
       presets: [
-        'babel-preset-expo',
-        // If you're using NativeWind v4, use it as a PRESET (not a plugin):
-        'nativewind/babel',
+        ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+        'nativewind/babel', // NativeWind v4 is a *preset*
       ],
       plugins: [
-        // Expo Router belongs in plugins
         'expo-router/babel',
-  
-        // Reanimated v3 uses the Worklets plugin – keep it LAST
-        'react-native-worklets/plugin',
+        'react-native-reanimated/plugin', // <-- LAST for Reanimated v3
       ],
     };
   };
